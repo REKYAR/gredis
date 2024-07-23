8b608cf4c5b625fe5ab9c9358a1fbe5e3c578544
REKYAR <mirgosmieszko@gmail.com>
2024-07-23 20:55:29 +0200

    multigoroutine tests

--- src/hash_table_custom_test.go
+++ src/hash_table_custom_test.go
@@ -59,7 +59,7 @@ func TestHashMapConcurrent(t *testing.T) {
 		log.Fatalf("Failed to create HMap: %v", err)
 	}
 
-	keys := []string{"key1", "key2", "key3"}
+	keys := []string{"key1", "key2", "key3", "key4", "key5", "key6", "key7", "key8", "key9", "key10"}
 	var wg sync.WaitGroup
 
 	// Concurrent Insertion
